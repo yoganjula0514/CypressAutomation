008e3cf17c6c383a8db76a80d241f33f509962ee
Gangarapu YoganjulaReddy MTTS <YoganjulaReddy.Gangarapu@mt.com>
2025-08-21 02:13:26 +0530

    Usage of each function when we have multiple child elements

--- cypress/e2e/FirstTest.cy.js
+++ cypress/e2e/FirstTest.cy.js
@@ -3,5 +3,23 @@ describe('Launch GreenKart', () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
     cy.get('.search-keyword').should(`be.visible`).type('ca')
     cy.get(`.product:visible`).should(`have.length`, 4) // Handling only visible elements
+
+    // Find element from parent child chaining    
+    cy.get(`.products .product`).should(`have.length`, 4)
+
+    // Find command: Get child elements from parent element
+    cy.get(`.products`).find(`.product`).should(`have.length`, 4)
+
+    // Select Add to cart for 2nd product
+    cy.get(`.products`).find(`.product`).eq(1).contains(`ADD TO CART`).click()
+
+    // each method usage(jQuery)
+    cy.get(`.products`).find(`.product`).each(($item, index, $list) => {
+      const actualProductName = $item.find(`h4.product-name`).text()
+      if (actualProductName.includes(`Cashews`)) {
+        cy.wrap($item).find(`button`).click()
+      }
+
+    })
   })
 })
